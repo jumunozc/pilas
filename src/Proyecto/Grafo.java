c7977ce8346cc76b86a037579b3848e001e74a84
julian <julian@julian>
2019-06-05 20:52:44 -0500

    avance

--- src/Proyecto/Grafo.java
+++ src/Proyecto/Grafo.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import  LinearDataStructures.*;
 
+
 public class Grafo {
 
 	public List listaNodos;
@@ -54,24 +55,115 @@ public class Grafo {
 
 
 		NodoGrafo mayorConexiones=(NodoGrafo)grafo.listaNodos.head;
-		
+
 		int mayor = mayorConexiones.conexion.length();
 		int usuario = mayorConexiones.dato;
-		
-		while (mayorConexiones!=null) {
-			
+
+		while (mayorConexiones != null) {
+
 			if(mayorConexiones.conexion.length()> mayor  ) {
+
 				mayor = mayorConexiones.conexion.length();
 				usuario = mayorConexiones.dato;
-				
+
 			}
-			
+
 			mayorConexiones = mayorConexiones.next;
-			
+
 		}
 		return "El usuario #: "+usuario+" tiene la mayor cantidad de conexiones equivalentes a: "+mayor;
 
+	}
 
+	public int promedioConexiones(Grafo grafo) {
+
+		NodoGrafo mayorConexiones=(NodoGrafo)grafo.listaNodos.head;
+
+		int mayor = mayorConexiones.conexion.length();
+
+
+		while (mayorConexiones != null) {
+
+			if(mayorConexiones.conexion.length()> mayor  ) {
+
+				mayor = mayorConexiones.conexion.length();
+
+			}
+
+			mayorConexiones = mayorConexiones.next;
+
+		}
+		return mayor/grafo.listaNodos.length();
+	}
+	public double desviacionEstandar(Grafo grafo)  {
+
+		NodoGrafo conexiones =( NodoGrafo)grafo.listaNodos.head;
+		int contador = 0;
+
+		int promedio = promedioConexiones(grafo);
+
+		while (conexiones!=null) {
+			int sumatoria = 0;
+
+
+			sumatoria=(int) Math.pow(conexiones.conexion.length()-promedio, 2);
+			contador = contador + sumatoria;
+
+
+			conexiones=conexiones.next;
+		}
+
+		int[]n = rangoConexiones(grafo);
+		double desviacionEstandar = Math.sqrt(contador/n[0]);
+
+		return desviacionEstandar;
 
 	}
+
+	public int[] rangoConexiones(Grafo grafo) {
+
+		NodoGrafo numConexiones =(NodoGrafo)grafo.listaNodos.head;
+		int conexion1 = 0;
+		int conexion2 = 0;
+		int conexion3 = 0;
+		int conexion4 = 0;
+		int conexion5 = 0;
+		int conexion6 = 0;
+		int conexion7 = 0;
+		
+		int[] contadorRango = new int[8];
+		
+		while (numConexiones!=null) {
+
+			if(numConexiones.conexion.length()<=10) {
+				conexion1++;
+			}else if(numConexiones.conexion.length()> 10 &&numConexiones.conexion.length()<=50) {
+				conexion2++;
+			}else if(numConexiones.conexion.length()> 50 &&numConexiones.conexion.length()<=100) {
+				conexion3++;
+			}else if(numConexiones.conexion.length()> 100 &&numConexiones.conexion.length()<=300) {
+				conexion4++;
+			}else if(numConexiones.conexion.length()> 300 &&numConexiones.conexion.length()<=600) {
+				conexion5++;
+			}else if(numConexiones.conexion.length()> 600 &&numConexiones.conexion.length()<=1200) {
+				conexion6++;
+			}else if(numConexiones.conexion.length()>1200) {
+				conexion7++;
+			}
+
+			numConexiones=numConexiones.next;
+		}
+		contadorRango[0] = conexion1;
+		contadorRango[1] = conexion2;
+		contadorRango[2] = conexion3;
+		contadorRango[3] = conexion4;
+		contadorRango[4] = conexion5;
+		contadorRango[5] = conexion6;
+		contadorRango[6] = conexion7;
+
+		return contadorRango;
+
+	}
+
 }
+
--- src/Proyecto/MainGrafo.java
+++ src/Proyecto/MainGrafo.java
@@ -1,8 +1,10 @@
 package Proyecto;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.OutputStreamWriter;
 
 public class MainGrafo {
 
@@ -10,30 +12,39 @@ public class MainGrafo {
 		try {
 			Grafo grafo = new Grafo();
 			String cadena;
-			
+
 			FileReader bf = new FileReader("C:\\Users\\julian\\git\\pilas\\src\\Proyecto\\prueba");
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 			BufferedReader br = new BufferedReader(bf);
 			cadena = br.readLine();
-	
+
 			while(cadena!=null) {
-				
-				String[] entrada = cadena.split(",");
-				
+
+				String[] entrada = cadena.split(",");	
+
 				int nodo = Integer.parseInt(entrada[0]);
 				int arista = Integer.parseInt(entrada[1]);
-				
+
 				grafo.conexionesGrafo(new NodoGrafo (nodo), new NodoArista(arista));
 				cadena=br.readLine();
+
 			}
+			
 			grafo.imprimirGrafo();
-			System.out.println();
-			System.out.println(grafo.mayorConexiones(grafo));
+			bw.write(" \n");
+			bw.write("El grafo tiene una medida de : "+(int)grafo.desviacionEstandar(grafo) +" nodos de dispersión.");
+			bw.write(" \n");
+			bw.write("El promedio de conexiones por usuario es de :"+grafo.promedioConexiones(grafo)+" conexiones.");
+			bw.write(" \n");
+			bw.write(grafo.mayorConexiones(grafo));
+			bw.write(" \n");
+			bw.flush();
 			br.close();
 
 		} catch (FileNotFoundException e) {
 			System.out.println("NO SE ENCONTRO EL ARCHIVO");
 		} catch(Exception es) {
-
+			System.out.println("ERROR");
 		}
 
 	}
